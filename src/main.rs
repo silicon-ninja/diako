211dd5608f01a80e963a0624baaf7fe6f9af5d52
silicon-ninja <srikanthkandarp23@gmail.com>
2022-05-22 12:32:04 +0530

    addde menu layouting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use colored::Colorize;
 fn main() {
-    println!("Hello, world!");
+    println!("");
+    println!("{}",format!("█▀▀ █ ▀█▀   █▀ █░█░█ █ ▀█▀ █▀▀ █░█ █▀▀ █▀█").purple());
+    println!("{}",format!("█▄█ █ ░█░   ▄█ ▀▄▀▄▀ █ ░█░ █▄▄ █▀█ ██▄ █▀▄").purple());
+    println!("---------------------------------------------");
+    println!("{}",format!("built by @silicon-ninja").yellow());
+    println!("");
+    println!("{}, {}", "Welcome to the Git Switcher".green(), "Please select an option:".green());
+    println!("");
+    println!("{}", "1. Github".green());
+    println!("{}", "2. Gitlab".green());
+    println!("{}", "3. Bitbucket".green());
+    println!("{}", "4. Exit".green());
+    println!("");
+    // println!(
+    //     "{}, {}, {}, {}, {}, {}, and some normal text.",
+    //     format!("Bold").bold(),
+    //     format!("Red").blue(),
+    //     format!("Yellow").yellow(),
+    //     format!("Green Strikethrough").green().strikethrough(),
+    //     format!("Blue Underline").blue().underline(),
+    //     format!("Purple Italics").purple().italic()
+    // );
 }
